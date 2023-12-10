9841fe758c7a7fe90c1b347ea2051385d16c8d89
David Hamilton <davidhamilton473@gmail.com>
2023-12-09 20:46:21 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production:false,
+    firebase : {
+        apiKey: "AIzaSyBfJMsjd2Mb8VEOmG9tfuGDYu6iGUYaFwI",
+        authDomain: "kanbanfire-85ac9.firebaseapp.com",
+        projectId: "kanbanfire-85ac9",
+        storageBucket: "kanbanfire-85ac9.appspot.com",
+        messagingSenderId: "1084916042688",
+        appId: "1:1084916042688:web:07d4add62cc29c1541d625",
+        measurementId: "G-LQWCJB51BC"
+      }
+};
+
+
